142d591aad2edf49101248b8d61e5d31146f911e
wdeforest23 <wdeforest23@cmc.edu>
2023-02-05 20:55:33 -0800

    fixed fixme

--- Fixme.py
+++ Fixme.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
+
 def evens(n):
     '''
     Returns a list of even numbers from 0 to n inclusive.
     '''
+    xs = range(n+1)
+    xs = filter(lambda x: x % 2 == 0, xs)
+    return list(xs)
